29fe734324956b822fa81fd54bdad52eea95f9b3
Admin <ducnamnguyen99@gmail.com>
2024-06-15 19:21:22 +0700

    Bai tap phuong trinh bac 2

---
+++ PTbac2/PTbac2/PTbac2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTbac2
+{
+   class PTbac2
+    {
+        private double a { get; set; }
+        private double b { get; set; }
+        private double c { get; set; }
+        private double delta;
+        private double x;
+        public PTbac2(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void Cal()
+        {
+            if (a != 0)
+            {
+                delta = Math.Pow(b, 2) - 4 * a * c;
+                if(delta > 0)
+                {
+                    Console.WriteLine($"Phương trình có 2 nghiệm phân biệt: \nx1 = {(-b + Math.Sqrt(delta)) / (2 * a)}" +
+                        $"\nx2 = {(-b - Math.Sqrt(delta)) / (2 * a)}");
+                } else if (delta == 0)
+                {
+                    Console.WriteLine($"Phương trình có nghiệm kép -b / 2 * a : {-b / (2 * a)}");
+                } else
+                {
+                    Console.WriteLine("Phương trình vô nghiệm");
+                }
+               
+
+            } else
+            {
+                Console.WriteLine("Phương trình trở thành bậc 1 có dạng: bx + c = 0");
+                if (b != 0)
+                {
+                    x = -c / b;
+                    Console.WriteLine($"ta có nghiệm là x = - c / b : {x} ");
+                } else
+                {
+                    if( c == 0)
+                    {
+                        Console.Write("Phương trình có vô số nghiệm");
+                    }
+                    else
+                    {
+                        Console.Write("Phương trình có vô nghiệm");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PTbac2/PTbac2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace PTbac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true) // vòng lặp vô hạn
+                {
+                    double[] arr = new double[3];
+
+                    int index = 0;
+
+                    foreach (double number in arr)
+                    {
+                        Console.WriteLine($"Nhập giá trị lần lượt cho a, b, c");
+
+                        arr[index] = double.Parse(Console.ReadLine());
+                        index++;
+                    }
+                    PTbac2 PT2 = new PTbac2(arr[0], arr[1], arr[2]);
+                    PT2.Cal();
+                    Console.WriteLine("Bạn có muốn thực hiện phép toán khác không? Y/N");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() != "y") // chuyển Y từ hoa về thường nếu có
+                    {
+                        break;// Trả lời bằng khác Y thì dừng chương trình.
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine("có lỗi xảy ra {0}", ex.Message);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
